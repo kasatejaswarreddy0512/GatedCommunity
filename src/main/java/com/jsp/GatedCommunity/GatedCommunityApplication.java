5943604fc423aea005bd55286c1200ea254c657f
kasatejaswarreddy0512 <ktrreddy64@gmail.com>
2025-05-06 18:50:26 +0530

    Initial commit

---
+++ src/main/java/com/jsp/GatedCommunity/GatedCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.jsp.GatedCommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatedCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatedCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsp/GatedCommunity/GatedCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsp.GatedCommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatedCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
